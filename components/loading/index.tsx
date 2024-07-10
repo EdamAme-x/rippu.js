f1749fdbb2778db4a9ef3205a2f9a788a915f4b0
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-10 18:58:06 +0900

    add

---
+++ components/loading/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from "ink";
+
+export function Loading() {
+    return <Text>Loading...</Text>;
+}
--- packages/create-rippu/types/files.ts
+++
@@ -1,4 +0,0 @@
-export interface FileStructure {
-    filepath: string,
-    content: string
-}
