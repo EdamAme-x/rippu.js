7e200ca467ff865265503422f12c00a66ea9d05b
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-10 19:41:28 +0900

    add

---
+++ build.ts
@@ -0,0 +1,19 @@
+/*
+  MIT License
+  Copyright (c) 2024 amex2189
+*/
+
+import path from "node:path";
+import { build } from "esbuild";
+
+await build({
+  entryPoints: ["./cli/index.ts"],
+  outfile: path.resolve("./dist/index.js"),
+  bundle: true,
+  platform: "node",
+  target: "esnext",
+  minify: true,
+  sourcemap: true,
+  allowOverwrite: true,
+  format: "esm",
+});
--- cli/index.ts
+++ cli/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import { a } from "./a";
+
+console.log("Rippu - [リップ] means lip-gloss💄 in Japanese.");
+for (let i = 0; i < 24; i++) {
+    console.log(i);
+}
+console.log("Fast, feature-rich, user-friendly, and above all, upscale UI ✨");
+console.log("Supports: Node.js, Deno, Bun and more :heart: and ", a);
