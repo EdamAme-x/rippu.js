77317e8ce17e73388cd6e9b08046285963dc5855
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-11 07:03:31 +0000

    add

--- components/loading/index.tsx
+++ components/loading/index.tsx
@@ -1,5 +1,21 @@
 import { Text } from "ink";
 
-export function Loading() {
+const variantTexts = {
+    "box": ["◰", "◳", "◲", "◱"],
+    "tao": ["◐", "◓", "◑", "◒"],
+    "snake": ["⊶", "⊷"],
+    "bounce": [".", "o", "O", "°", "O", "o", "."],
+    "side-bounce": ["ဝ", "၀"],
+    "arrow": ["←", "↖", "↑", "↗", "→", "↘", "↓", "↙"],
+    "clock": ["🕐 ", "🕑 ", "🕒 ", "🕓 ", "🕔 ", "🕕 ", "🕖 ", "🕗 ", "🕘 ", "🕙 ", "🕚 "],
+    "pipe": ["\\", "|", "/", "-"],
+    "point": ["⠙", "⠘", "⠰", "⠴", "⠤", "⠦", "⠆", "⠃", "⠋", "⠉"]
+} satisfies Record<string, string[]>
+
+export function Loading({
+    variant = "point"
+}: {
+    variant: keyof typeof variantTexts
+}) {
     return <Text>Loading...</Text>;
 }
