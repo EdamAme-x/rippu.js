a778ffe46cfb6ba3f95f7042b52786e0a84ed0f3
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-12 01:30:44 +0000

    lint

--- cli/index.tsx
+++ cli/index.tsx
@@ -1,30 +1,30 @@
-import { render } from "ink";
-import type { RenderOptions } from "ink";
+import { render } from 'ink'
+import type { RenderOptions } from 'ink'
 
-import AddCommand from "./commands/add";
-import HelpCommand from "./commands/help";
-import PrefetchCommand from "./commands/prefetch";
-import UnknownCommand from "./commands/unknown";
+import AddCommand from './commands/add'
+import HelpCommand from './commands/help'
+import PrefetchCommand from './commands/prefetch'
+import UnknownCommand from './commands/unknown'
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
 
-const [command, ...params] = args;
+const [command, ...params] = args
 
 const commonOptions: RenderOptions = {
 	patchConsole: false
-};
+}
 
 switch (command) {
-	case "help":
-		render(<HelpCommand command={command} params={params} />, commonOptions);
-		break;
-	case "add":
-		render(<AddCommand command={command} params={params} />, commonOptions);
-		break;
-	case "prefetch":
-		render(<PrefetchCommand command={command} params={params} />, commonOptions);
-		break;
+	case 'help':
+		render(<HelpCommand command={command} params={params} />, commonOptions)
+		break
+	case 'add':
+		render(<AddCommand command={command} params={params} />, commonOptions)
+		break
+	case 'prefetch':
+		render(<PrefetchCommand command={command} params={params} />, commonOptions)
+		break
 	default:
-		render(<UnknownCommand command={command} params={params} />, commonOptions);
-		break;
+		render(<UnknownCommand command={command} params={params} />, commonOptions)
+		break
 }
