9086567ae56a03433a0e647c736344a8fa7f573a
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-12 03:20:04 +0000

    update

--- cli/commands/add/index.tsx
+++ cli/commands/add/index.tsx
@@ -1,6 +1,23 @@
 import { Text } from 'ink'
+import Logger from '../../../components/logger';
+
+const fileTypes = {
+	"jsx": "jsx",
+	"tsx": "tsx"
+}
 
 const AddCommand = (props: { command: string; params: string[] }) => {
+	if (props.params.length < 1) {
+		return <Logger type="error" message={"Not enough arguments"} />
+	}
+	const componentName = props.params[0].toLowerCase();
+	const putPath = props.params[1] ?? `./${componentName}.tsx`;
+	let fileType = putPath.split(".").pop() ?? "tsx";
+
+	if (!(fileType in fileTypes)) {
+		fileType = "tsx";
+	}
+
 	return <Text>WIP</Text>
 }
 
