85c772afecf1fa8b210d8aacc43e42c1612c3d69
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-13 14:16:54 +0900

    add tree

--- components/tree.tsx
+++ components/tree.tsx
@@ -1,4 +1,4 @@
-import { render, Text } from 'ink'
+import { Text } from 'ink'
 
 type Tree = {
 	text: string | JSX.Element
@@ -42,23 +42,44 @@ function getPipe(
 	return TreePipes[type][bold ? 'bold' : 'regular']
 }
 
+/**
+ * Renders a tree structure recursively.
+ *
+ * @param {TreeRoot} tree - The root tree node.
+ * @param {boolean} [bold=false] - Flag to determine if text should be bold.
+ * @param {string} [color='default'] - The color of the text.
+ * @param {number} [space=1] - The space between tree branches.
+ * @param {number} [length=1] - The length of each branch.
+ * @param {number} [between=0] - The space between branches.
+ */
 export function Tree({
 	tree,
 	bold = false,
 	color = 'default',
 	space = 1,
 	length = 1,
+	between = 0,
 }: {
 	tree: TreeRoot
 	bold?: boolean
 	color?: string
 	space?: number
 	length?: number
+	between?: number
 }) {
 	return (
 		<>
 			{tree.map((branch, index) => (
-				<Branch key={index} tree={branch} bold={bold} color={color} space={space} length={length} isLast={index === tree.length - 1} />
+				<Branch
+					key={index}
+					tree={branch}
+					bold={bold}
+					color={color}
+					space={space}
+					length={length}
+					isLast={index === tree.length - 1}
+					between={between}
+				/>
 			))}
 		</>
 	)
@@ -70,23 +91,30 @@ function Branch({
 	color = 'white',
 	space,
 	length,
+	between,
 	depth = 0,
-    isLast
+	isLast,
 }: {
 	tree: Tree
 	bold: boolean
 	color: string
 	space: number
 	length: number
+	between: number
 	depth?: number
-    isLast: boolean
+	isLast: boolean
 }) {
 	return (
 		<>
+			{Array.from({ length: between }).map((_, i) => (
+				<Text key={i}>{(getPipe('vertical', bold) + ' '.repeat(length)).repeat(depth + 1)}</Text>
+			))}
 			<Text>
 				<Text color={color}>{(getPipe('vertical', bold) + ' '.repeat(length)).repeat(depth)}</Text>
 				<Text color={color}>
-					{isLast ? getPipe('halfVerticalAndHorizontal', bold) :  getPipe('verticalAndHorizontal', bold)}
+					{isLast
+						? getPipe('halfVerticalAndHorizontal', bold)
+						: getPipe('verticalAndHorizontal', bold)}
 					{getPipe('horizontal', bold).repeat(length)}
 					{tree.children ? getPipe('horizontalAndVertical', bold) : getPipe('horizontal', bold)}
 					{getPipe('horizontal', bold)}
@@ -101,40 +129,15 @@ function Branch({
 							color={color}
 							space={space + 1}
 							length={length}
+							between={between}
 							depth={depth + 1}
 							key={index}
-                            isLast={tree.children ? index === tree.children.length - 1 : false}
+							isLast={tree.children ? index === tree.children.length - 1 : false}
 						/>
 					))
 				: null}
 		</>
 	)
 }
-
-render(
-	<Tree
-		tree={[
-			{
-				text: 'a',
-				children: [
-					{
-						text: 'b',
-						children: [
-							{
-								text: 'c',
-							},
-						],
-					},
-					{
-						text: 'd',
-					},
-				],
-			},
-			{
-				text: 'e',
-			},
-		]}
-	/>
-)
 
 // #rippu:Tree:A Tree Component:ink
