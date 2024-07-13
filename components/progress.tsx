89ebd84bd96a1761630b3c40b8a4f760ef0c687a
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-13 15:24:14 +0900

    fix progress

--- components/progress.tsx
+++ components/progress.tsx
@@ -8,6 +8,15 @@ const progressMap = {
 	pipe: ['', '|'],
 } as const satisfies Record<string, string[]>
 
+/**
+ * A function that renders a progress bar based on the input parameters.
+ *
+ * @param {number} progress - The progress value (default is 0, which means 0% and maximum is 100).
+ * @param {number} length - The length of the progress bar (default is 20).
+ * @param {keyof typeof progressMap} type - The type of progress bar to render.
+ * @param {string} color - The color of the progress bar (default is 'default').
+ * @return {JSX.Element} The JSX element representing the rendered progress bar.
+ */
 export function Progress({
 	progress = 0,
 	length = 20,
@@ -19,7 +28,7 @@ export function Progress({
 	type?: keyof typeof progressMap
 	color?: string
 }) {
-	const progressPercentage = progress > 100 ? 100 : progress
+	const progressPercentage = progress > 100 ? 100 : progress < 0 ? 0 : progress
 	const progressLengthFloat = (progressPercentage / 100) * length
 	const progressLength = Math.floor(progressLengthFloat)
 	const progressRemaining = Math.round(
